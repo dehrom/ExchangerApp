ef95b126ebef32c339228b9f3f97b4944c4ff1c5
Валерий Коканов <vkokanov@alfabank.ru>
2018-07-20 21:24:29 +0300

    Initial Commit

---
+++ ExchangerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangerApp
+//
+//  Created by Валерий Коканов on 20.07.2018.
+//  Copyright © 2018 my-night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
